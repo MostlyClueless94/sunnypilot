7e161b2005f7bb358d1629530388cb99c5779ccb
tonesto7 <tonesto7@users.noreply.github.com>
2026-02-21 16:41:33 -0500

    moved sentry over new bp instance

--- system/sentry.py
+++ system/sentry.py
@@ -20,7 +20,9 @@ CRASHES_DIR = Paths.crash_log_root()
 
 class SentryProject(Enum):
   # python project
-  SELFDRIVE = "https://186a6736b7927e5ae9b92c869ba81b6b@o1138119.ingest.us.sentry.io/4508660076052480"
+  # SELFDRIVE = "https://186a6736b7927e5ae9b92c869ba81b6b@o1138119.ingest.us.sentry.io/4508660076052480"
+  SELFDRIVE = "https://dd96fe788b05e1b9a59fb68bd7604314@o4510926103379968.ingest.us.sentry.io/4510926105739264"
+
   # native project
   SELFDRIVE_NATIVE = SELFDRIVE
 

2bb3d7c937cefc3ae9a535c66ecfb6bd8427d333
MostlyClueless94 <damentroutman@hotmail.com>
2026-03-05 22:57:35 -0600

    subaru: expose outback 2025 in selector

--- opendbc/car/subaru/interface.py
+++ opendbc/car/subaru/interface.py
@@ -106,7 +106,7 @@ class CarInterface(CarInterfaceBase):
   def _get_params_sp(stock_cp: structs.CarParams, ret: structs.CarParamsSP, candidate, fingerprint: dict[int, dict[int, int]],
                      car_fw: list[structs.CarParams.CarFw], alpha_long: bool, is_release_sp: bool, docs: bool) -> structs.CarParamsSP:
     stock_cp.dashcamOnly = bool((stock_cp.flags & SubaruFlags.HYBRID) or
-                                ((stock_cp.flags & SubaruFlags.LKAS_ANGLE) and is_release_sp))
+                                ((stock_cp.flags & SubaruFlags.LKAS_ANGLE) and is_release_sp and not docs))
 
     if candidate == CAR.SUBARU_FORESTER_2022:
       stock_cp.dashcamOnly = True
